3ba8b9eae1f9b308327ca2bc5204042e75463934
Navid Rojiani <nrojiani@gmail.com>
2016-02-22 22:39:56 -0800

    End of Lecture 29

--- todos/src/header.jsx
+++ todos/src/header.jsx
@@ -2,12 +2,31 @@ var React     = require('react');
 var ReactDOM  = require('react-dom');
 
 module.exports = React.createClass({
+  getInitialState: function () {
+    return {
+      text: ''
+    }
+  },
+  handleClick: function () {
+    // Send value of text input to Firebase
+    console.log(this.state.text);
+  },
+  handleInputChange: function (event) {
+    this.setState({ text: event.target.value });
+  },
   render: function () {
     return (
       <div className="input-group">
-        <input type="text" className="form-control" />
+        <input
+          className="form-control"
+          onChange={this.handleInputChange}
+          type="text"
+          value={this.state.text} />
         <span className="input-group-btn">
-          <button className="btn btn-default" type="button">
+          <button
+            onClick={this.handleClick}
+            className="btn btn-default"
+            type="button">
             Add
           </button>
         </span>
