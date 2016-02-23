cd34fe04cdead606a00f93f0dc67133f72754a22
Navid Rojiani <nrojiani@gmail.com>
2016-02-22 22:28:58 -0800

    End of Lecture 28

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -2,23 +2,25 @@ var React     = require('react');
 var ReactDOM  = require('react-dom');
 var ReactFire = require('reactfire');   // Bridge between Firebase & React components
 var Firebase  = require('firebase');    // Firebase library
+var Header    = require('./header');
 var rootUrl   = 'https://blinding-heat-4098.firebaseio.com/';
 
 
 var App = React.createClass({
   mixins: [ ReactFire ], // copies code from this object onto our React component
   componentWillMount: function () {
-    /* Creates a one-way binding from node in your Firebase database to an
-       object in this.state of your React component. The name of the object
-       stored in this.state is specified using the bindVar variable. */
     this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
-    // this.state.items ==> { }
   },
   render: function () {
     return (
-      <h1 className="red">
-        Hello, React!
-      </h1>
+      <div className="row panel panel-default">
+        <div className="col-md-8 col-md-offset-2">
+          <h2 className="text-center">
+            To-Do List
+          </h2>
+          <Header />
+        </div>
+      </div>
     );
   }
 });
