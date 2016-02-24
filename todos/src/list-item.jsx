1d3a8adc2308aa7b889666b39b415b688022138d
Navid Rojiani <nrojiani@gmail.com>
2016-02-23 21:37:40 -0800

    Lecture 34: Send checked/done to Firebase Data Store

--- todos/src/list-item.jsx
+++ todos/src/list-item.jsx
@@ -1,17 +1,33 @@
 var React     = require('react');
 var ReactDOM  = require('react-dom');
+var rootUrl   = 'https://blinding-heat-4098.firebaseio.com/';
 
 module.exports = React.createClass({
+  componentWillMount: function () {
+    // Create Firebase reference so we can update an item's "done" property
+    // in handleDoneChange
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
+  },
   getInitialState: function () {
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
+  },
+  handleDoneChange: function (event) {
+    var update = {done: event.target.checked}
+    this.setState(update);
+    this.fb.update(update);
   },
   render: function () {
     return (
       <div className="input-group">
         <span className="input-group-addon">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={this.state.done}
+            onChange={this.handleDoneChange}
+          />
         </span>
         <input type="text"
           className="form-control"
