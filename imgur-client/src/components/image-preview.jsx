cd988e147f2747525b8f3dab2c457e1cfd8793e5
Navid Rojiani <nrojiani@gmail.com>
2016-02-25 19:38:22 -0800

    62: Playing videos on Mouseover

--- imgur-client/src/components/image-preview.jsx
+++ imgur-client/src/components/image-preview.jsx
@@ -2,15 +2,36 @@ var React        = require('react');
 var ReactDOM     = require('react-dom');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      hovering: false
+    }
+  },
   render: function () {
     return (
-      <div className="image-preview">
-        {this.image()}
+      <div
+        className="image-preview"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}>
+        {this.props.animated && this.state.hovering ? this.video() : this.image()}
       </div>
     );
   },
   image: function () {
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg'; // image preview
     return <img src={link} />
+    },
+  video: function() {
+    return <div>
+      <video preload='auto' autoPlay='autoplay' loop='loop' webkit-playsinline>
+        <source src={this.props.mp4} type='video/mp4'></source>
+      </video>
+    </div>
+  },
+  handleMouseEnter: function() {
+    this.setState({hovering: true});
+  },
+  handleMouseLeave: function() {
+    this.setState({hovering: false});
   }
 });
