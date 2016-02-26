2d87cab40adfd08b367a3d6018dbd005ff0d5825
Navid Rojiani <nrojiani@gmail.com>
2016-02-25 19:56:19 -0800

    64: Image stats overlay

--- imgur-client/src/components/header.jsx
+++ imgur-client/src/components/header.jsx
@@ -19,24 +19,28 @@ module.exports = React.createClass({
     Actions.getTopics();
   },
   render: function () {
-    return <nav className="navbar navbar-default header">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          Imgur Browser
-        </Link>
-        <ul className="nav navbar-nav navbar-right">
-          {this.renderTopics()}
-        </ul>
-      </div>
-    </nav>
+    return (
+      <nav className="navbar navbar-default header">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            Imgur Browser
+          </Link>
+          <ul className="nav navbar-nav navbar-right">
+            {this.renderTopics()}
+          </ul>
+        </div>
+      </nav>
+    );
   },
   renderTopics: function () {
     return this.state.topics.slice(0, 4).map(function (topic){
-      return <li key={topic.id}>
-        <Link activeClassName="active" to={"topics/" + topic.id}>
-          {topic.name}
-        </Link>
-      </li>
+      return (
+        <li key={topic.id}>
+          <Link activeClassName="active" to={"topics/" + topic.id}>
+            {topic.name}
+          </Link>
+        </li>
+      );
     });
   },
   onChange: function (event, topics) {
--- imgur-client/src/components/image-preview.jsx
+++ imgur-client/src/components/image-preview.jsx
@@ -15,22 +15,36 @@ module.exports = React.createClass({
         onMouseLeave={this.handleMouseLeave}>
         {this.props.animated && this.state.hovering ? this.video() : this.image()}
         {this.props.animated && !this.state.hovering ? this.icon() : null }
+        {this.state.hovering ? this.inset() : null}
+      </div>
+    );
+  },
+  inset: function() {
+    return (
+      <div className="inset">
+        Views: {this.props.views}
+        <br />
+        Upvotes: {this.props.ups}
       </div>
     );
   },
   image: function () {
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg'; // image preview
-    return <img src={link} />
-    },
+    return (<img src={link} />);
+  },
   video: function() {
-    return <div>
-      <video preload='auto' autoPlay='autoplay' loop='loop' webkit-playsinline>
-        <source src={this.props.mp4} type='video/mp4'></source>
-      </video>
-    </div>
+    return (
+      <div>
+        <video preload='auto' autoPlay='autoplay' loop='loop' webkit-playsinline>
+          <source src={this.props.mp4} type='video/mp4'></source>
+        </video>
+      </div>
+    );
   },
   icon: function() {
-    return <span className="glyphicon glyphicon-play"></span>
+    return (
+      <span className="glyphicon glyphicon-play"></span>
+    );
   },
   handleMouseEnter: function() {
     this.setState({hovering: true});
