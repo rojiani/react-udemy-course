ce5a645aba379ba4d4ce988bc1d5ecd8f0a92abf
Navid Rojiani <nrojiani@gmail.com>
2016-02-25 18:03:08 -0800

    58: Implement ImageStore

--- imgur-client/src/components/topic.jsx
+++ imgur-client/src/components/topic.jsx
@@ -1,10 +1,30 @@
 var React       = require('react');
 var ReactDOM    = require('react-dom');
+var Reflux      = require('reflux');
+var Actions     = require('../actions');
+var ImageStore  = require('../stores/image-store');
+
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+  componentWillMount: function () {
+    Actions.getImages(this.props.params.id);
+  },
+  getInitialState: function () {
+    return {
+      images: []
+    }
+  },
   render: function () {
     return (
-      <div>I am a topic</div>
+      <div>
+        I am a topic with ID {this.props.params.id}
+      </div>
     );
+  },
+  onChange: function (event, images) {
+    this.setState({images: images});
   }
 });
